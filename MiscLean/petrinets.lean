41a3569e9ea9d0570695cc41fccbc058fb177514
mdnestor <mdnest01@gmail.com>
2024-10-11 17:09:11 -0400

    add petri nets

--- MiscLean/petrinets.lean
+++ MiscLean/petrinets.lean
@@ -2,15 +2,17 @@
 -- categories of nets https://arxiv.org/pdf/2101.04238
 
 import Mathlib.Data.Multiset.Basic
+import Mathlib.Logic.Function.Defs
 import Mathlib.CategoryTheory.Category.Basic
 
 open CategoryTheory
 
+-- definition 4.1
 class PreNet where
-  specie: Type
-  trans: Type
-  source: transition → List specie
-  target: transition → List specie
+  specie: Type u
+  trans: Type v
+  source: trans → List specie
+  target: trans → List specie
 
 class PreNet.map (P1 P2: PreNet) where
   smap: P1.specie → P2.specie
@@ -21,21 +23,35 @@ class PreNet.map (P1 P2: PreNet) where
 def PreNet.id (P: PreNet): PreNet.map P P := {
   smap := fun x => x
   tmap := fun x => x
-  h1 := sorry
-  h2 := sorry
+  h1 := by ext; simp
+  h2 := by ext; simp
 }
 
 def PreNet.comp {P1 P2 P3: PreNet} (f1: PreNet.map P1 P2) (f2: PreNet.map P2 P3): PreNet.map P1 P3 := {
   smap := f2.smap ∘ f1.smap
   tmap := f2.tmap ∘ f1.tmap
-  h1 := sorry
-  h2 := sorry
+  h1 := by
+    rw [←Function.comp.assoc]
+    rw [←f2.h1]
+    rw [Function.comp.assoc]
+    rw [←f1.h1]
+    rw [←Function.comp.assoc]
+    rw [List.map_comp_map] -- diagram chasing is less fun
+  h2 := by
+    rw [←Function.comp.assoc]
+    rw [←f2.h2]
+    rw [Function.comp.assoc]
+    rw [←f1.h2]
+    rw [←Function.comp.assoc]
+    rw [List.map_comp_map]
 }
 
+-- prenets form a category
 instance: Category PreNet where
   Hom := PreNet.map
   id := PreNet.id
   comp := PreNet.comp
+  -- axioms deduced by `aesop_cat` tactic
 
 class PetriNet where
   specie: Type
@@ -59,15 +75,31 @@ def PreNet.toPetriNet (P: PreNet): PetriNet := {
 def PetriNet.id (P: PetriNet): PetriNet.map P P := {
   smap := fun x => x
   tmap := fun x => x
-  h1 := sorry
-  h2 := sorry
+  h1 := by ext; simp
+  h2 := by ext; simp
 }
+
+lemma Multiset.map_comp_map: Multiset.map f ∘ Multiset.map g = Multiset.map (f ∘ g) := by
+  ext
+  apply map_map
 
 def PetriNet.comp {P1 P2 P3: PetriNet} (f1: PetriNet.map P1 P2) (f2: PetriNet.map P2 P3): PetriNet.map P1 P3 := {
   smap := f2.smap ∘ f1.smap
   tmap := f2.tmap ∘ f1.tmap
-  h1 := sorry
-  h2 := sorry
+  h1 := by
+    rw [←Function.comp.assoc]
+    rw [←f2.h1]
+    rw [Function.comp.assoc]
+    rw [←f1.h1]
+    rw [←Function.comp.assoc]
+    rw [←Multiset.map_comp_map]
+  h2 := by
+    rw [←Function.comp.assoc]
+    rw [←f2.h2]
+    rw [Function.comp.assoc]
+    rw [←f1.h2]
+    rw [←Function.comp.assoc]
+    rw [←Multiset.map_comp_map]
 }
 
 instance: Category PetriNet where
