be5ea37f1b59b5c685a6a2959b82b82bb105cc9e
mdnestor <mdnest01@gmail.com>
2024-10-17 07:48:58 +0000

    Update spec.lean

--- MiscLean/spec.lean
+++ MiscLean/spec.lean
@@ -20,10 +20,20 @@ class CommRing (X: Type u) where
   mul_assoc: ∀ x y z: X, mul (mul x y) z = mul x (mul y z)
   mul_one: ∀ x: X, mul x one = x
   one_mul: ∀ x: X, mul one x = x
+  mul_comm: ∀ x y: X, mul x y = mul y x
   left_distrib: ∀ x y z: X, mul x (add y z) = add (mul x y) (mul x z)
   right_distrib: ∀ x y z: X, mul (add x y) z = add (mul x z) (mul y z)
 
 open CommRing
+
+lemma zero_mul {X: Type u} [CommRing X] (x: X): mul zero x = zero := by
+  calc
+    mul zero x = add (mul zero x) zero := by rw [add_zero]
+             _ = add (mul zero x) (add (mul zero x) (inv (mul zero x))) := by rw [add_inv_right]
+             _ = add (add (mul zero x) (mul zero x)) (inv (mul zero x)) := by rw [add_assoc]
+             _ = add (mul (add zero zero) x) (inv (mul zero x)) := by rw [right_distrib]
+             _ = add (mul zero x) (inv (mul zero x)) := by rw [add_zero]
+             _ = zero := by rw [add_inv_right]
 
 class Ideal {X: Type u} [CommRing X] (I: Set X): Prop where
   h1: zero ∈ I
@@ -56,24 +66,48 @@ theorem univ_ideal {X: Type u} [CommRing X]: Ideal (@Set.univ X) := {
   h3 := by simp
 }
 
+-- V(X) = ∅ 
 theorem V_univ {X: Type u} [CommRing X]: V (@Set.univ X) = ∅ := by
-  ext ⟨P, hP⟩
+  ext ⟨_, hP⟩
   constructor
-  intro h
-  simp_all
-  sorry
-  simp
+  . intro
+    simp_all
+    have := hP.h5.right
+    contradiction
+  . intro
+    simp_all
 
+-- V({0}) = PrimeIdeals(X)
 theorem V_zero {X: Type u} [CommRing X]: V (Set.singleton (zero: X)) = Set.univ := by
   ext P
   constructor
-  intro h
+  intro
   simp
   simp
-  sorry
+  intro _ hx
+  rw [Set.eq_of_mem_singleton hx]
+  exact P.2.h1
 
-def Spec {X: Type u} [CommRing X]: TopologicalSpace (PrimeIdeals X) := {
-  opensets := Set.image (fun I: Set X => (V I)ᶜ) (Ideals X)
+theorem V_union {X: Type u} [CommRing X] (I: Set (Set X)): V (Set.sUnion I) = Set.sInter (Set.image V Set.univ) := by
+  ext P
+  constructor
+  intro h S hS
+  obtain ⟨a, ha⟩ := hS
+  simp at ha
+  rw [←ha]
+  intro x hx
+
+  simp
+
+  simp
+  simp
+  intro _ hx
+  rw [Set.eq_of_mem_singleton hx]
+  exact P.2.h1
+
+
+def Spectrum {X: Type u} [CommRing X]: TopologicalSpace (PrimeIdeals X) := {
+  opensets := Set.image (fun I => (V I)ᶜ) (Ideals X)
   empty_isOpen := by
     exists {zero}
     constructor
@@ -81,12 +115,12 @@ def Spec {X: Type u} [CommRing X]: TopologicalSpace (PrimeIdeals X) := {
       simp
       intros
       simp_all
-      sorry
-      intros
+      apply add_zero
+      intro _ y _
       simp_all
-      sorry
-    . sorry
-      -- simp [Set.compl_empty]
+      rw [zero_mul]
+    . simp
+      exact V_zero
   univ_isOpen := by
     exists Set.univ
     constructor
@@ -95,8 +129,21 @@ def Spec {X: Type u} [CommRing X]: TopologicalSpace (PrimeIdeals X) := {
     rw [V_univ]
   union_isOpen := by
     intro S h
+    simp
+    simp [Set.image] at h
+    let f: S → Ideals X := by
+      intro s
+      have h1 := h s.prop
+      simp at h1
+      let y := Classical.choose h1
+      have hy := Classical.choose_spec h1
+      exact ⟨y, hy.left⟩
+    let Is := Set.image f Set.univ
+    -- ∃ x ∈ Ideals X, (V x)ᶜ = ⋃₀ S
     sorry
   inter_isOpen := by
     intro S h
+    simp
+    -- ∃ x ∈ Ideals X, (V x)ᶜ = ⋂₀ S
     sorry
 }
