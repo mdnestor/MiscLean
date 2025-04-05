166ecf8b74a8ba9b0436faeb29e32735c24c65f3
mdnestor <mdnest01@gmail.com>
2025-04-05 16:43:42 +0000

    Update bernoulli_measure.lean

--- MiscLean/bernoulli_measure.lean
+++ MiscLean/bernoulli_measure.lean
@@ -36,3 +36,12 @@ noncomputable def BernoulliMeasure {X: Type u} {f: X → ENNReal} (hf: ∑' x, f
 
 noncomputable def BernoulliProbabilityMeasure {X: Type u} {f: X → ENNReal} (hf: ∑' x, f x = 1) (I: Type v): ProbabilityMeasure (I → X) :=
   ⟨BernoulliMeasure hf I, MeasureTheory.Measure.instIsProbabilityMeasureForallInfinitePi (hμ := fun _ => discrete_measure_is_probability_measure hf)⟩
+
+def shift (X: Type u) {T: Type v} [Mul T] (t0: T): (T → X) → (T → X) :=
+  fun f => fun t => f (t0 * t)
+
+-- next I want to prove this is a measure preserving transformation
+theorem Bernoulli_shift_measure_preserving {X: Type u} {f: X → ENNReal} (hf: ∑' x, f x = 1) {T: Type v} [Group T] (t0: T): MeasureTheory.MeasurePreserving (shift X t0) (BernoulliMeasure hf T) (BernoulliMeasure hf T) := {
+  measurable := by sorry
+  map_eq := by sorry
+}
