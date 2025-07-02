0d71cee228d05e8f11bd8071cb18ae1a8dacdc66
mdnestor <mdnest01@gmail.com>
2025-07-02 17:12:16 +0000

    Update borel_negligible_dense_complement.lean

--- MiscLean/borel_negligible_dense_complement.lean
+++ MiscLean/borel_negligible_dense_complement.lean
@@ -35,41 +35,42 @@ def negligible_dense_compl (m: Measure X): Prop :=
   ∀ E, MeasurableSet E → m E = 0 → Dense Eᶜ
 
 def strictly_positive (m: Measure X): Prop :=
-  ∀ U, IsOpen U → Nonempty U → m U ≠ 0
+  ∀ U, IsOpen U → U.Nonempty → m U ≠ 0
 
 theorem negligible_dense_compl_iff (m: Measure X): negligible_dense_compl m ↔ strictly_positive m := by
   constructor
-  . intro h U hU1 hU2
-    have hU3 := h U (MeasurableSpace.measurableSet_generateFrom hU1)
-    have: ¬Dense Uᶜ := by
-      simp [dense_iff_inter_open]
+  . intro h U U_open U_nonempty
+    contrapose h
+    simp [negligible_dense_compl]
+    exists U
+    repeat' constructor
+    . exact U_open
+    . exact Function.notMem_support.mp h
+    . simp [dense_iff_inter_open]
       exists U
-      constructor
-      exact hU1
-      constructor
-      exact nonempty_subtype.mp hU2
-      simp
-    exact fun a => this (hU3 a)
-  . intro h E hE1 hE2
+      repeat' (apply And.intro)
+      . exact U_open
+      . exact U_nonempty
+      . simp
+  . intro h E _ E_negl
     apply by_contradiction
     intro h1
     simp [dense_iff_inter_open] at h1
-    obtain ⟨U, hU1, hU2, hU3⟩ := h1
-    have := h U hU1 (by exact Set.Nonempty.to_subtype hU2)
-    have: U ⊆ E := by
-      intro x hx
+    obtain ⟨U, U_open, U_nonempty, U_inter_E_compl_nonempty⟩ := h1
+    have m_U_pos := h U U_open U_nonempty
+    have U_subset_E: U ⊆ E := by
+      intro x x_in_U
       apply by_contradiction
-      intro hx2
-      have hx3: x ∈ U ∩ Eᶜ := Set.mem_inter hx hx2
-      have: (U ∩ Eᶜ) = ∅ := by exact Set.not_nonempty_iff_eq_empty.mp hU3
-      rw [this] at hx3
-      exact hx3
-    have: m E > 0 :=
-      calc
-        m E ≥ m U := by exact OuterMeasureClass.measure_mono m (this)
-          _ > 0   := measure_pos_of_superset (fun ⦃a⦄ a => a) (h U hU1 (Set.Nonempty.to_subtype hU2))
+      intro x_notin_E
+      have x_in_U_inter_E_compl: x ∈ U ∩ Eᶜ := Set.mem_inter x_in_U x_notin_E
+      have: U ∩ Eᶜ = ∅ := Set.not_nonempty_iff_eq_empty.mp U_inter_E_compl_nonempty
+      rw [this] at x_in_U_inter_E_compl
+      contradiction
+    have: m E > 0 := calc
+      m E ≥ m U := by exact OuterMeasureClass.measure_mono m U_subset_E
+        _ > 0   := measure_pos_of_superset (by rfl) m_U_pos
     have: m E ≠ 0 := Ne.symm (ne_of_lt this)
-    exact this hE2
+    contradiction
 
 -- corollary
 example {X: Type u} {Y: Type v} [TopologicalSpace X] [TopologicalSpace Y] [T2Space Y] {f g: X → Y} {hf: Continuous f} {hg: Continuous g} {D: Set X} {hD0: MeasurableSet D} {hD1: ∀ x ∈ D, f x = g x} {m: Measure X} {hm: strictly_positive m} {hD2: m Dᶜ = 0}: f = g := by
