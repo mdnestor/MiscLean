27986dc78d368981942e651befdb7a16c936670d
mdnestor <mdnest01@gmail.com>
2025-07-06 15:43:54 -0400

    Update sheaf.lean

--- MiscLean/sheaf.lean
+++ MiscLean/sheaf.lean
@@ -5,7 +5,7 @@ import Mathlib.CategoryTheory.ConcreteCategory.Basic
 
 open TopologicalSpace CategoryTheory
 
--- convenient helpers, not sure why these aren't in mathlib
+-- helpers for working with `Opens`, not sure why these aren't in mathlib
 instance (X: Type*) [TopologicalSpace X]: HasSubset (Opens X) := {
   Subset := fun U V => U.carrier ⊆ V.carrier
 }
@@ -25,17 +25,13 @@ structure Presheaf (X C: Type*) [TopologicalSpace X] [Category C] where
   comp {U V W: Opens X} (UV: U ⊆ V) (VW: V ⊆ W): res VW ≫ res UV = res (subset_trans UV VW)
 
 -- simple example of a presheaf: the set of functions U → Y where Y is some type
+-- uses the (large) category instance `CategoryTheory.types`
 example (X: Type u) (Y: Type v) [TopologicalSpace X]: Presheaf X (Type (max u v)) := {
   sect := fun U => U → Y
-  res := by
-    intro _ _ UV f ⟨x, hx⟩
-    apply f
-    apply Subtype.mk
-    exact UV hx
+  res := fun UV f ⟨x, hx⟩ => f ⟨x, UV hx⟩
   res_refl := by intro; rfl
   comp := by intros; rfl
 }
-
 
 -- define presheaf homomorphism
 structure Presheaf.hom {X C: Type*} [TopologicalSpace X] [Category C] (F G: Presheaf X C) where
@@ -59,7 +55,7 @@ def Presheaf.hom_comp {X C: Type*} [TopologicalSpace X] [Category C] {F1 F2 F3: 
 -- turns out to talk about individual sections, we need to reference the underlying type of each section
 -- so we need to invoke all this ugly code about concrete categogries
 -- I only the index of the cover to be universe level 0
-structure Sheaf (X C: Type*) [TopologicalSpace X] [Category C] {FC : C → C → Type*} {CC : C → Type*} [(X Y : C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] extends Presheaf X C where
+structure Sheaf (X C: Type*) [TopologicalSpace X] [Category C] {FC: C → C → Type*} {CC: C → Type*} [(X Y: C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] extends Presheaf X C where
   locality {U: Opens X} {I: Type} {V: I → Opens X} {s t: CC (sect U)}
     (h1: ∀ i, (V i) ⊆ U)
     (h2: U = Set.iUnion (fun i => (V i).carrier))
@@ -70,9 +66,20 @@ structure Sheaf (X C: Type*) [TopologicalSpace X] [Category C] {FC : C → C →
     (h3: ∀ i j, @res (V i ∩ V j) (V i) (fun _ => Set.mem_of_mem_inter_left) (s i) = @res (V i ∩ V j) (V j) (fun _ => Set.mem_of_mem_inter_right) (s j)):
     ∃ t: CC (sect U), ∀ i, @res (V i) U (fun _ a => h1 i a) t = s i
 
+-- turns out the category of types is not a concrete category (???) so here is another version using the hasforget instance
+structure Sheaf2 (X C: Type*) [TopologicalSpace X] [Category C] [HasForget C] extends Presheaf X C where
+  locality {U: Opens X} {I: Type} {V: I → Opens X} {s t: (forget C).obj (sect U)}
+    (h1: ∀ i, (V i) ⊆ U)
+    (h2: U = Set.iUnion (fun i => (V i).carrier))
+    (h3: ∀ i, (forget C).map (res (h1 i)) s = (forget C).map (res (h1 i)) t): s = t
+  gluing {U: Opens X} {I: Type} {V: I → Opens X} {s: (i: I) → (forget C).obj (sect (V i))}
+    (h1: ∀ i, (V i) ⊆ U)
+    (h2: U = Set.iUnion (fun i => (V i).carrier))
+    (h3: ∀ i j, (forget C).map (@res (V i ∩ V j) (V i) (fun _ => Set.mem_of_mem_inter_left)) (s i) = (forget C).map (@res (V i ∩ V j) (V j) (fun _ => Set.mem_of_mem_inter_right)) (s j)):
+    ∃ t: (forget C).obj (sect U), ∀ i, (forget C).map (@res (V i) U (fun _ a => h1 i a)) t = s i
+
 -- show the section obtains by the gluing axioms is unique
--- (basically a strengthening of the gluing axiom)
-theorem Sheaf.glue_unique (X C: Type*) [TopologicalSpace X] [Category C] {FC : C → C → Type*} {CC : C → Type*} [(X Y : C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] (F: Sheaf X C)
+theorem Sheaf.glue_unique (X C: Type*) [TopologicalSpace X] [Category C] {FC: C → C → Type*} {CC: C → Type*} [(X Y: C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] (F: Sheaf X C)
   {U: Opens X} {I: Type} {V: I → Opens X} (s: (i: I) → CC (F.sect (V i)))
   (h1: ∀ i, (V i) ⊆ U)
   (h2: U = Set.iUnion (fun i => (V i).carrier))
