a742c6922f5d95313a85a74b241f2ef58619d8d3
mdnestor <mdnest01@gmail.com>
2025-07-06 13:08:37 -0400

    Update sheaf.lean

--- MiscLean/sheaf.lean
+++ MiscLean/sheaf.lean
@@ -21,19 +21,69 @@ instance (X: Type*) [TopologicalSpace X]: Inter (Opens X) := {
 structure Presheaf (X C: Type*) [TopologicalSpace X] [Category C] where
   sect (U: Opens X): C
   res {U V: Opens X} (h: U ⊆ V): sect V ⟶ sect U
-  id (U: Opens X): res (subset_refl U.carrier) = 𝟙 (sect U)
+  res_refl (U: Opens X): res (subset_refl U.carrier) = 𝟙 (sect U)
   comp {U V W: Opens X} (UV: U ⊆ V) (VW: V ⊆ W): res VW ≫ res UV = res (subset_trans UV VW)
+
+-- simple example of a presheaf: the set of functions U → Y where Y is some type
+example (X: Type u) (Y: Type v) [TopologicalSpace X]: Presheaf X (Type (max u v)) := {
+  sect := fun U => U → Y
+  res := by
+    intro _ _ UV f ⟨x, hx⟩
+    apply f
+    apply Subtype.mk
+    exact UV hx
+  res_refl := by intro; rfl
+  comp := by intros; rfl
+}
+
+
+-- define presheaf homomorphism
+structure Presheaf.hom {X C: Type*} [TopologicalSpace X] [Category C] (F G: Presheaf X C) where
+  map: (U: Opens X) → F.sect U ⟶ G.sect U
+  compat: ∀ U V: Opens X, (UV: U ⊆ V) → F.res UV ≫ map U = map V ≫ G.res UV
+
+-- show the identity is a presheaf morphism
+def Presheaf.id {X C: Type*} [TopologicalSpace X] [Category C] (F: Presheaf X C): Presheaf.hom F F := {
+  map := fun U => 𝟙 (F.sect U)
+  compat := by intro; simp
+}
+
+-- define composition of presheaf morphisms
+def Presheaf.hom_comp {X C: Type*} [TopologicalSpace X] [Category C] {F1 F2 F3: Presheaf X C} (φ1: Presheaf.hom F1 F2) (φ2: Presheaf.hom F2 F3): Presheaf.hom F1 F3 := {
+  map := fun U => φ1.map U ≫ φ2.map U
+  compat := by
+    intro _ _ UV
+    simp [←Category.assoc (F1.res UV), φ1.compat, Category.assoc, φ2.compat]
+}
 
 -- turns out to talk about individual sections, we need to reference the underlying type of each section
 -- so we need to invoke all this ugly code about concrete categogries
+-- I only the index of the cover to be universe level 0
 structure Sheaf (X C: Type*) [TopologicalSpace X] [Category C] {FC : C → C → Type*} {CC : C → Type*} [(X Y : C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] extends Presheaf X C where
-  locality {U: Opens X} {I: Type w} {V: I → Opens X} (s t: CC (sect U))
+  locality {U: Opens X} {I: Type} {V: I → Opens X} {s t: CC (sect U)}
     (h1: ∀ i, (V i) ⊆ U)
     (h2: U = Set.iUnion (fun i => (V i).carrier))
     (h3: ∀ i, res (h1 i) s = res (h1 i) t): s = t
-  gluing {U: Opens X} {I: Type w} {V: I → Opens X} (s: (i: I) → CC (sect (V i)))
+  gluing {U: Opens X} {I: Type} {V: I → Opens X} {s: (i: I) → CC (sect (V i))}
     (h1: ∀ i, (V i) ⊆ U)
     (h2: U = Set.iUnion (fun i => (V i).carrier))
     (h3: ∀ i j, @res (V i ∩ V j) (V i) (fun _ => Set.mem_of_mem_inter_left) (s i) = @res (V i ∩ V j) (V j) (fun _ => Set.mem_of_mem_inter_right) (s j)):
     ∃ t: CC (sect U), ∀ i, @res (V i) U (fun _ a => h1 i a) t = s i
+
+-- show the section obtains by the gluing axioms is unique
+-- (basically a strengthening of the gluing axiom)
+theorem Sheaf.glue_unique (X C: Type*) [TopologicalSpace X] [Category C] {FC : C → C → Type*} {CC : C → Type*} [(X Y : C) → FunLike (FC X Y) (CC X) (CC Y)] [ConcreteCategory C FC] (F: Sheaf X C)
+  {U: Opens X} {I: Type} {V: I → Opens X} (s: (i: I) → CC (F.sect (V i)))
+  (h1: ∀ i, (V i) ⊆ U)
+  (h2: U = Set.iUnion (fun i => (V i).carrier))
+  (h3: ∀ i j, @F.res (V i ∩ V j) (V i) (fun _ => Set.mem_of_mem_inter_left) (s i) = @F.res (V i ∩ V j) (V j) (fun _ => Set.mem_of_mem_inter_right) (s j)):
+  ∃! t: CC (F.sect U), ∀ i, @F.res (V i) U (fun _ a => h1 i a) t = s i := by
+  obtain ⟨t, ht⟩ := F.gluing h1 h2 h3
+  exists t
+  constructor
+  . exact ht
+  . intro t' ht'
+    apply F.locality h1 h2
+    intro
+    simp_all [Opens.carrier_eq_coe]
 
